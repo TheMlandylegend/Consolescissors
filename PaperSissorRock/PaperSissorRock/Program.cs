2a399cbcfd5f38fdef834edc8c23480d03aeb467
mlandy09 <mc_louigene@yahoo.com>
2020-05-15 12:13:50 -0400

    Add new fie date 05/15/2020

---
+++ PaperSissorRock/PaperSissorRock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaperSissorRock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
